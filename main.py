673fcf4a015bfa0b481b3825718b29de479c4ee3
mangart <zan.gostic1@gmail.com>
2026-01-26 12:46:36 +0100

    dodane funkcije za ponudbo

--- main.py
+++ main.py
@@ -43,6 +43,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+def read_root():
+    return {"Mikrostoritev": "PoslovalnicaZaposleni"}
+
 # Zacetek Avtoservis
 
 @app.get("/avtoservis/{idtennant}")
@@ -270,18 +274,201 @@ def posodobi_poslovalnico(posl: Poslovalnica1):
 
 # Konec poslovalnice
 
+# Zacetek ponudba
+
+# zacetek Classi
+
+class Ponudba(BaseModel):
+    idposlovalnica: str
+    idstoritev: str
+    idtennant: str
+    uniqueid: str
+
+class Ponu(BaseModel):
+    idtennant: str
+    uniqueid: str
+    
+class Ponu1(BaseModel):
+    idponudba: str
+    idtennant: str
+    uniqueid: str
+    
+class Ponudba1(BaseModel):
+    idponudba: str
+    idposlovalnica: str
+    idstoritev: str
+    aktiven: str
+    idtennant: str
+    uniqueid: str
+
+# konec Classi
 
 
-@app.get("/")
-def read_root():
-    return {"Mikrostoritev": "PoslovalnicaZaposleni"}
+@app.post("/dodajponudbo/")
+def dodajPonudbo(ponudba: Ponudba):
+    userid = ponudba.uniqueid
+    try:
+        conn = pool.get_connection()
+        # Create a cursor
+        cursor = conn.cursor()
+        query = "SELECT IDTennant, TennantDBPoslovalnice FROM  " + adminbaza + ".TennantLookup WHERE IDTennant = %s"
+        cursor.execute(query,(ponudba.idtennant,))
+        row = cursor.fetchone()
+        if row is None:
+            raise HTTPException(status_code=404, detail="DB not found")
+        tennantDB = row[1]
+        
+        query = "INSERT INTO " + tennantDB + ".Ponuja(IDPoslovalnica,IDStoritev,Aktiven) VALUES (%s,%s,%s)"
+        cursor.execute(query,(ponudba.idposlovalnica,ponudba.idstoritev,1))
+        return {"Ponudba": "passed"}
+  
+    except Exception as e:
+        print("Error: ", e)
+        return {"Ponudba": "failed"}
+    finally:
+        cursor.close()
+        conn.close() 
+    return {"Ponudba": "unknown"}    
+    
+    
+@app.post("/ponudbe/")
+def get_ponudbe(ponu: Ponu):
+    userid = ponu.uniqueid
+    try:
+        with pool.get_connection() as conn:
+            with conn.cursor() as cursor:
+                # get tennant db
+                query = "SELECT IDTennant, TennantDBPoslovalnice FROM  " + adminbaza + ".TennantLookup WHERE IDTennant = %s"
+                cursor.execute(query,(ponu.idtennant,))
+                row = cursor.fetchone()
+                if row is None:
+                    raise HTTPException(status_code=404, detail="DB not found")
+                tennantDB = row[1]
+                
+                cursor.execute("SELECT IDStoritev FROM "+ tennantDB +".Ponuja")
+                rows = cursor.fetchall()
+                storitve_ids = list({
+                row[0]
+                for row in rows
+                if row[0] is not None
+                })
+                print(storitve_ids)
+                fail = 0
+                try:
+                    data = {"ids": storitve_ids, "uniqueid": ponu.uniqueid}
+                    response = requests.post(f"{SERVICE_ADMVOZ_URL}/izbranestoritve/", json=data, timeout=5)
+                    #response.raise_for_status()  # Raise exception for HTTP errors  
+                    print(response)
+                    if "application/json" not in response.headers.get("Content-Type", ""):
+                        sql = "SELECT pp.IDPoslovalnica, pp.IDStoritev, p.NazivPoslovalnice, pp.Aktiven FROM " + tennantDB + ".Ponuja pp, " + tennantDB + ".Poslovalnica p WHERE p.IDPoslovalnica = pp.IDPoslovalnica"
+                        cursor.execute()
+                        rows = cursor.fetchall()
+                        # Fixed columns → no need to read cursor.description
+                        return [
+                            {"IDPoslovalnica": row[0], "IDStoritev": row[1], "NazivPoslovalnice": row[2], "Aktiven": row[3], "NazivStoritve": None}
+                            for row in rows
+                        ]
+                    else:
+                        result = response.json()
+                        print(result)
+                        sql = "SELECT pp.IDPoslovalnica, pp.IDStoritev, p.NazivPoslovalnice, pp.Aktiven FROM " + tennantDB + ".Ponuja pp, " + tennantDB + ".Poslovalnica p WHERE p.IDPoslovalnica = pp.IDPoslovalnica"
+                        cursor.execute(sql)
+                        rows = cursor.fetchall()
+                        # Fixed columns → no need to read cursor.description
+                        return [
+                            {"IDPoslovalnica": row[0], "IDStoritev": row[1], "NazivPoslovalnice": row[2], "Aktiven": row[3], "NazivStoritve": result.get(str(row[1]))}
+                            for row in rows
+                        ]
+                except Exception as e:
+                    print("Prislo je do napake: ", e)
+                    fail = 1
+                if fail == 1:
+                    sql = "SELECT pp.IDPoslovalnica, pp.IDStoritev, p.NazivPoslovalnice, pp.Aktiven FROM " + tennantDB + ".Ponuja pp, " + tennantDB + ".Poslovalnica p WHERE p.IDPoslovalnica = pp.IDPoslovalnica"
+                    cursor.execute(sql)
+                    rows = cursor.fetchall()
+                    # Fixed columns → no need to read cursor.description
+                    return [
+                        {"IDPoslovalnica": row[0], "IDStoritev": row[1], "NazivPoslovalnice": row[2], "Aktiven": row[3], "NazivStoritve": None}
+                        for row in rows
+                    ]
+    except Exception as e:
+        print("DB error:", e)
+        #raise HTTPException(status_code=500, detail="Database error")
+    return {"Ponudba": "failed"} 
 
-@app.get("/items/")
-def read_items():
-    return {"Tu": "So izdelki"}
+class Posl1(BaseModel):
+    idposlovalnica: str
+    idtennant: str
+    uniqueid: str
+
+@app.post("/ponudba/")
+def get_ponudba(ponu1: Ponu1):
+
+    try:
+        with pool.get_connection() as conn:
+            with conn.cursor() as cursor:
+                #get baza poslovalnic
+                query = "SELECT IDTennant, TennantDBPoslovalnice FROM  " + adminbaza + ".TennantLookup WHERE IDTennant = %s"
+                cursor.execute(query,(ponu1.idtennant,))
+                row = cursor.fetchone()
+                if row is None:
+                    raise HTTPException(status_code=404, detail="DB not found")
+                tennantDB = row[1]
+                
+                sql = "SELECT IDPonuja, IDPoslovalnica, IDStoritev, Aktiven FROM " + tennantDB + ".Ponuja WHERE IDPonudbe = %s"
+                cursor.execute(sql,(ponu1.idponudba,))
+
+                row = cursor.fetchone()
+
+                if row is None:
+                    raise HTTPException(status_code=404, detail="Kraj not found")
+
+                return {
+                    "IDPoslovalnica": row[0],
+                    "IDPoslovalnica": row[1],
+                    "IDStoritev": row[2],
+                    "Aktiven": row[3]
+                }
+
+    except HTTPException:
+        raise
+    except Exception as e:
+        print("DB error:", e)
+        raise HTTPException(status_code=500, detail="Database error")
+    return {"Ponudba": "undefined"}
+
+@app.put("/posodobiponudbo/")
+def posodobi_ponudbo(ponu: Ponudba1):
+    userid = ponu.uniqueid
+    try:
+        conn = pool.get_connection()
+        # Create a cursor
+        cursor = conn.cursor()
+        query = "SELECT IDTennant, TennantDBPoslovalnice FROM  " + adminbaza + ".TennantLookup WHERE IDTennant = %s"
+        cursor.execute(query,(ponu.idtennant,))
+        row = cursor.fetchone()
+        if row is None:
+            raise HTTPException(status_code=404, detail="DB not found")
+        tennantDB = row[1]
+        
+        if ponu.aktiven != '1':
+            ponu.aktiven = '0'
+        query = "UPDATE "+tennantDB+".Ponuja SET IDPoslovalnica = %s, IDStoritev = %s, Aktiven = %s WHERE IDPonudba = %s"
+        cursor.execute(query,(ponu.idposlovalnica,ponu.idstoritev,ponu.aktiven,ponu.idponudba))
+        return {"Ponudba": "passed"}
+  
+    except Exception as e:
+        print("Error: ", e)
+        return {"Ponudba": "failed"}
+    finally:
+        cursor.close()
+        conn.close() 
+    return {"Ponudba": "unknown"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# Konec ponudba
+
+
+
+
     
